db52c961a9498e0013d0f4c75d5ef4436623b5b9
derekhmd <derekhmd@gmail.com>
2019-09-30 14:01:39 -0700

    Added Elastic Search index print

---
+++ clarivate_es_mapping.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ssh -f derek.ahme@bastion.dev.oneplatform.build -L 9245:elasticsearch.qa1.us-west-2.migration.dev.oneplatform.build:9200 -N
+curl -X GET "localhost:9245/author-20197280/_mapping?pretty"
